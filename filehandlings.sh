e2bb4e291336636549cee12addfa6de440100aef
Aishwarya Bhand <aishwaryabhand.devops@gmail.com>
2025-12-21 11:41:13 +0530

    added word count

--- filehandlings.sh
+++ filehandlings.sh
@@ -4,10 +4,15 @@
 
 #check if file argument is provided by user
 filename=$1
+word=$2
 if [[ $# -eq 0 ]];then
         echo "usage: ./script.sh <filename>"
 	exit 0
 fi
+
+
+
+#checking sixe of file
 if [[ -f $filename ]];then
         echo "File size: $(ls -lh $filename | awk '{print $5}')"
 
@@ -16,6 +21,19 @@ if [[ -f $filename ]];then
 
         # echo "$(wc -c $filename)"
         # or echo "$(stat -c %s $filename)"
+
+
+#Count the number of lines containing the word "xyz"
+
+	count=$(grep -o $word $filename | wc -l)
+	echo "$word word count: $count"
+	if [[  $count -gt 0 ]];then
+		echo "word found! Check immediately."
+	else
+		echo "word not found"
+	fi
+	
+
 else
         echo "file does not exist"
         echo "$(touch /c/Users/91801/Desktop/work/linux_projects/$filename)$filename:file created succesfully"
